e3d1abff01b8b65fffd3f2081217842ea48791cd
TuanAnhhh123 <147674430+TuanAnhhh123@users.noreply.github.com>
2023-11-07 20:32:31 +0700

    ss2C

---
+++ bai tap 1 ss2C.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	float square_side , width , length , radius;
+	printf("Nhap do dai hinh vuong : ");
+	scanf("%f" , &square_side );
+	printf("Nhap chieu rong , chieu dai hinh chu nhat : ");
+	scanf("%f %f ", &width , &length);
+	printf("Nhap ban kinh cua hinh tron : "),
+	scanf("%f ", &radius);
+	const float Pi = 3.14;
+	float square_perimeter = square_side * 4;
+	float square_area = square_side * square_side;
+	float rectangle_perimeter = (width + length) * 2;
+	float rectangle_aera = width * length;
+	float circle_perimeter = radius * 2 * Pi;
+	float circle_area = radius * radius * Pi;
+	printf("Chu vi va dien tich hinh vuong la : %f %f \n" , square_perimeter , square_area);
+	printf("Chu vi va dien tich hinh chu nhat la : %f %f \n" , rectangle_perimeter , rectangle_area);
+	printf("Chu vi va dien tich hinh tron la : %f %f \n" , circle_perimeter , circle_area);  
+}
